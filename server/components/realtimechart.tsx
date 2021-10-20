790cd676ce873d21f5b92b81385b98c36f019a9d
hamatatsu <6399551+hamatatsu@users.noreply.github.com>
2021-10-20 07:14:39 +0000

    Split realtimechart and add date label

---
+++ server/components/realtimechart.tsx
@@ -0,0 +1,55 @@
+import ApexChart from 'apexcharts'
+import { NextPage } from 'next'
+import dynamic from 'next/dynamic'
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
+
+const tooltipFormatter = (value: number): string => {
+  const d = new Date(value)
+  return d.toLocaleTimeString('ja-JP') + `.${d.getMilliseconds()}`
+}
+const labelFormatter = (value: string, timestamp: number): string => {
+  const d = new Date(timestamp)
+  return d.toLocaleTimeString('ja-JP') + `.${d.getMilliseconds()}`
+}
+const options = {
+  chart: {
+    zoom: {
+      enabled: false
+    },
+    animations: {
+      easing: "linear",
+      dynamicAnimation: {
+        speed: 500
+      }
+    }
+  },
+  tooltip: {
+    x: {
+      formatter: tooltipFormatter
+    }
+  },
+  xaxis: {
+    type: "datetime",
+    range: 5000,
+    labels: {
+      formatter: labelFormatter
+    }
+  },
+  yaxis: {
+    labels: {
+      formatter: val => val.toFixed(0)
+    },
+    title: { text: "Value" },
+  }
+} as ApexChart.ApexOptions
+
+interface Props { dataList: Array<{ name: string, data: Array<{ x: number, y: number }> }> }
+
+const RealtimeChart: NextPage<Props> = (props) => (<Chart
+  options={options}
+  series={props.dataList}
+  type="line"
+  width={1000} />)
+
+export default RealtimeChart
--- server/pages/test.tsx
+++ server/pages/test.tsx
@@ -1,10 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import ApexChart from 'apexcharts'
-import { useEffect, useState } from 'react'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import RealtimeChart from '../components/realtimechart';
 
-const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 const Test: NextPage = () => {
   const nameList = ["a"];
@@ -14,46 +12,13 @@ const Test: NextPage = () => {
   } as { name: string, data: Array<{ x: number, y: number }> }));
   const [dataList, setDataList] = useState(defaultDataList)
 
-  const options = {
-    chart: {
-      zoom: {
-        enabled: false
-      },
-      animations: {
-        easing: "linear",
-        dynamicAnimation: {
-          speed: 500
-        }
-      }
-    },
-    tooltip: {
-      x: {
-        format: "yyyy/MM/dd HH:mm:ss.fff"
-      }
-    },
-    xaxis: {
-      type: "datetime",
-      range: 30000
-    },
-    yaxis: {
-      labels: {
-        formatter: val => val.toFixed(0)
-      },
-      title: { text: "Value" }
-    }
-  } as ApexChart.ApexOptions
   useEffect(() => {
     const addDataRandomly = (data: Array<{ x: number, y: number }>) => {
-      // 指定した確率`ADDING_DATA_RATIO`でデータを追加する
-      // if (Math.random() < 1 - 0.8) {
-      //   return data;
-      // }
-
       return [
         ...data,
         {
           x: Date.now(),
-          y: data.length * Math.random() // データの量に応じて最大値が増えるランダムな数
+          y: 500 * Math.random() // データの量に応じて最大値が増えるランダムな数
         }
       ];
     };
@@ -66,8 +31,7 @@ const Test: NextPage = () => {
           };
         })
       );
-    }, 500);
-
+    }, 20);
     return () => clearInterval(interval);
   }, [dataList])
 
@@ -81,11 +45,8 @@ const Test: NextPage = () => {
       <div className="app">
         <div className="row">
           <div className="mixed-chart">
-            <Chart
-              options={options}
-              series={dataList}
-              type="line"
-              width={1000}
+            <RealtimeChart
+              dataList={dataList}
             />
           </div>
         </div>
