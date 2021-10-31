80ad097a9e7c8c1b9b7882a5487d947b0b0ae554
hamatatsu <6399551+hamatatsu@users.noreply.github.com>
2021-10-31 10:43:41 +0000

    Add data to db

--- server/index.ts
+++ server/index.ts
@@ -4,13 +4,26 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
+const pool = new Pool();
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  client.query(
+    'DROP TABLE IF EXISTS test CASCADE; CREATE TABLE test (id serial, date integer, leg smallint, PRIMARY KEY (id));',
+    (err, result) => {
+      release();
+      if (err) console.error('Error executing query', err.stack);
+      console.log(result.rows);
+    }
+  );
+});
+
 const mqttClient = connect('mqtt://host.docker.internal', {
   username: process.env.MQTTUSER,
   password: process.env.MQTTPASSWORD,
 });
-
-const pool = new Pool();
-pool.connect();
 
 mqttClient.on('connect', () => {
   mqttClient.subscribe('presence', (err) => {
@@ -18,25 +31,26 @@ mqttClient.on('connect', () => {
       mqttClient.publish('presence', 'Hello mqtt');
     }
   });
+  mqttClient.subscribe('test');
 });
 mqttClient.on('error', (err) => console.error(err));
 mqttClient.on('message', (topic, message) => {
-  // message is Buffer
-  console.log(message.toString());
-  mqttClient.end();
-});
-
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error('Error acquiring client', err.stack);
+  if (topic === 'presence') console.log(message.toString());
+  if (topic === 'test') {
+    const json = JSON.parse(message.toString());
+    pool.connect((err, client, release) => {
+      client.query(
+        `INSERT INTO test (date, leg) values (${json['date']}, ${json['leg']})`,
+        (err) => {
+          release();
+          if (err) console.error('Error executing query', err.stack);
+        }
+      );
+    });
   }
-  client.query('SELECT NOW()', (err, result) => {
-    release();
-    if (err) {
-      return console.error('Error executing query', err.stack);
-    }
-    console.log(result.rows);
-  });
 });
 
-pool.end();
+process.on('beforeExit', () => {
+  mqttClient.end();
+  pool.end();
+});
